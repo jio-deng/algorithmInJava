27d727f15293d6b98e4ebf5215344162b4b5c048
dengziming <501823206@qq.co>
2019-12-10 13:00:31 +0800

    [Add] LeetCode 001, 002, 043, 167

---
+++ lib/src/main/java/com/dengzm/lib/leetcode/ListNode.java
@@ -0,0 +1,18 @@
+package com.dengzm.lib.leetcode;
+
+/**
+ * @author Johnny Deng
+ * @version 1.0
+ * @description List node
+ * @date 2019/12/10 11:24
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode() {}
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
