9f5f9ff9473cb27f69f835e0ba80ececff458b1e
Johnny Deng <40154415+jio-deng@users.noreply.github.com>
2019-12-15 18:30:47 +0800

    [Add] LeetCode 011, 013
    Signed-off-by: dengziming <501823206@qq.co>

---
+++ lib/src/main/java/com/dengzm/lib/leetcode/Leetcode011.java
@@ -0,0 +1,44 @@
+package com.dengzm.lib.leetcode;
+
+/**
+ * @author Johnny Deng
+ * @version 1.0
+ * @description 盛最多水的容器
+ * @date 2019/12/15 17:25
+ */
+public class Leetcode011 {
+    /**
+     * 这道题我最开始陷入了一个思维误区绕了半天，其实就是记住盛水量由最短板决定即可
+     * 必须要去改变短板位置，否则向中心收拢时乘积只会越来越小
+     */
+
+    public int maxArea(int[] height) {
+        if (height == null || height.length < 2) {
+            return 0;
+        }
+
+        int max = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            int minHeight = Math.min(height[left], height[right]);
+            int ans = (right - left) * minHeight;
+            if (ans > max) {
+                max = ans;
+            }
+
+            if (height[left] < height[right]) {
+                left ++;
+            } else {
+                right --;
+            }
+        }
+
+        return max;
+    }
+
+    /*执行结果：通过
+      显示详情
+      执行用时 :3 ms, 在所有 java 提交中击败了93.11%的用户
+      内存消耗 :39.9 MB, 在所有 java 提交中击败了92.44%的用户*/
+}
