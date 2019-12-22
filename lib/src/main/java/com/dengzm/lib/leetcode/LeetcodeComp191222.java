468a3682f902d0b883a44f0afe0761d0cf660f72
Johnny Deng <40154415+jio-deng@users.noreply.github.com>
2019-12-22 16:42:36 +0800

    [Add] LeetCode Comp 2019.12.22
    Signed-off-by: dengziming <501823206@qq.co>

--- lib/src/main/java/com/dengzm/lib/leetcode/LeetcodeComp191222.java
+++ lib/src/main/java/com/dengzm/lib/leetcode/LeetcodeComp191222.java
@@ -1,16 +1,22 @@
 package com.dengzm.lib.leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 /**
  * @author Johnny Deng
  * @version 1.0
- * @description TODO
+ * @description 竞赛
  * @date 2019/12/22 10:26
  */
 public class LeetcodeComp191222 {
     /**
-     * 5291. 统计位数为偶数的数字 显示英文描述
+     * 5291. 统计位数为偶数的数字
      * 用户通过次数0
      * 用户尝试次数0
      * 通过次数0
@@ -68,7 +74,7 @@ public class LeetcodeComp191222 {
 
 
     /**
-     * 5292. 划分数组为连续数字的集合 显示英文描述
+     * 5292. 划分数组为连续数字的集合
      * 用户通过次数3
      * 用户尝试次数3
      * 通过次数3
@@ -144,7 +150,7 @@ public class LeetcodeComp191222 {
     }
 
     /**
-     * 5293. 子串的最大出现次数 显示英文描述
+     * 5293. 子串的最大出现次数
      * 用户通过次数60
      * 用户尝试次数98
      * 通过次数60
@@ -184,67 +190,156 @@ public class LeetcodeComp191222 {
      * 1 <= minSize <= maxSize <= min(26, s.length)
      * s 只包含小写英文字母。
      */
+    // 自己写的超时了 学习了大佬的解法
+    /*作者：NPE_TLE
+    链接：https://leetcode-cn.com/problems/maximum-number-of-occurrences-of-a-substring/solution/java-hashmap-by-npe_tle/
+    来源：力扣（LeetCode）
+    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*/
     public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
-        if (s == null || s.length() == 0 || s.length() < minSize) {
-            return 0;
-        }
-
-        int result = 0;
-        int flag = 0;
-
-        for (int i = 0; i < s.length() - minSize; i ++) {
-            for (int j = minSize; j <= maxSize; j ++) {
-                if (i + j > s.length()) {
-                    break;
-                }
-                String target = s.substring(i, i + j);
-                if (hasMaxLetters(target, maxLetters)) {
-                    int temp = findString(s.substring(i), target);
-                    flag = 1;
-                    if (temp > result) {
-                        result = temp;
-                    }
-
-                }
-            }
-        }
-
-        return result == 0 && flag == 1 ? 1 : result;
-    }
-
-    public boolean hasMaxLetters(String target, int maxLetters) {
-        int re = 0;
-        int[] result = new int[26];
-        for (int i = 0; i < target.length(); i ++) {
-            if (result[target.charAt(i) - 'a'] ++ == 0) {
-                re ++;
-            }
-        }
-
-        return re <= maxLetters;
-    }
-
-    public int findString(String target, String pattern) {
-        int result = 0;
-        for (int i = 0; i < target.length(); i ++) {
-            if (i + pattern.length() > target.length()) {
+        int len = s.length();
+        Map<String, Integer> countMap = new HashMap<>();
+        for (int i = 0; i < len; i++) {
+            if (i + minSize > len) {
                 break;
             }
-//            System.out.println(pattern);
-//            System.out.println(target.substring(i, i + pattern.length()));
-//            System.out.println();
-            if (target.charAt(i) == pattern.charAt(0) && target.substring(i, i + pattern.length()).equals(pattern)) {
-                result ++;
+
+            String sub = s.substring(i, i + minSize);
+            if (isMatch(sub, maxLetters)) {
+                countMap.put(sub, countMap.getOrDefault(sub, 0) + 1);
             }
         }
 
-        return result;
+        int ansMax = 0;
+        for (String str: countMap.keySet()) {
+            int count = countMap.get(str);
+            if (count > ansMax) {
+                ansMax = count;
+            }
+        }
+
+        return ansMax;
+    }
+
+    private boolean isMatch(String str, int maxLetters) {
+        Set<Character> set = new HashSet<>();
+        for (char c : str.toCharArray()) {
+            set.add(c);
+            if (set.size() > maxLetters) {
+                return false;
+            }
+        }
+
+        return set.size() <= maxLetters;
     }
 
 
+    /**
+     * 5294. 你能从盒子里获得的最大糖果数
+     * 给你 n 个盒子，每个盒子的格式为 [status, candies, keys, containedBoxes] ，其中：
+     *
+     * 状态字 status[i]：整数，如果 box[i] 是开的，那么是 1 ，否则是 0 。
+     * 糖果数 candies[i]: 整数，表示 box[i] 中糖果的数目。
+     * 钥匙 keys[i]：数组，表示你打开 box[i] 后，可以得到一些盒子的钥匙，每个元素分别为该钥匙对应盒子的下标。
+     * 内含的盒子 containedBoxes[i]：整数，表示放在 box[i] 里的盒子所对应的下标。
+     * 给你一个 initialBoxes 数组，表示你现在得到的盒子，你可以获得里面的糖果，也可以用盒子里的钥匙打开新的盒子，还可以继续探索从这个盒子里找到的其他盒子。
+     *
+     * 请你按照上述规则，返回可以获得糖果的 最大数目 。
+     *
+     *  
+     *
+     * 示例 1：
+     *
+     * 输入：status = [1,0,1,0], candies = [7,5,4,100], keys = [[],[],[1],[]], containedBoxes = [[1,2],[3],[],[]], initialBoxes = [0]
+     * 输出：16
+     * 解释：
+     * 一开始你有盒子 0 。你将获得它里面的 7 个糖果和盒子 1 和 2。
+     * 盒子 1 目前状态是关闭的，而且你还没有对应它的钥匙。所以你将会打开盒子 2 ，并得到里面的 4 个糖果和盒子 1 的钥匙。
+     * 在盒子 1 中，你会获得 5 个糖果和盒子 3 ，但是你没法获得盒子 3 的钥匙所以盒子 3 会保持关闭状态。
+     * 你总共可以获得的糖果数目 = 7 + 4 + 5 = 16 个。
+     * 示例 2：
+     *
+     * 输入：status = [1,0,0,0,0,0], candies = [1,1,1,1,1,1], keys = [[1,2,3,4,5],[],[],[],[],[]], containedBoxes = [[1,2,3,4,5],[],[],[],[],[]], initialBoxes = [0]
+     * 输出：6
+     * 解释：
+     * 你一开始拥有盒子 0 。打开它你可以找到盒子 1,2,3,4,5 和它们对应的钥匙。
+     * 打开这些盒子，你将获得所有盒子的糖果，所以总糖果数为 6 个。
+     * 示例 3：
+     *
+     * 输入：status = [1,1,1], candies = [100,1,100], keys = [[],[0,2],[]], containedBoxes = [[],[],[]], initialBoxes = [1]
+     * 输出：1
+     * 示例 4：
+     *
+     * 输入：status = [1], candies = [100], keys = [[]], containedBoxes = [[]], initialBoxes = []
+     * 输出：0
+     * 示例 5：
+     *
+     * 输入：status = [1,1,1], candies = [2,3,2], keys = [[],[],[]], containedBoxes = [[],[],[]], initialBoxes = [2,1,0]
+     * 输出：7
+     *  
+     *
+     * 提示：
+     *
+     * 1 <= status.length <= 1000
+     * status.length == candies.length == keys.length == containedBoxes.length == n
+     * status[i] 要么是 0 要么是 1 。
+     * 1 <= candies[i] <= 1000
+     * 0 <= keys[i].length <= status.length
+     * 0 <= keys[i][j] < status.length
+     * keys[i] 中的值都是互不相同的。
+     * 0 <= containedBoxes[i].length <= status.length
+     * 0 <= containedBoxes[i][j] < status.length
+     * containedBoxes[i] 中的值都是互不相同的。
+     * 每个盒子最多被一个盒子包含。
+     * 0 <= initialBoxes.length <= status.length
+     * 0 <= initialBoxes[i] < status.length
+     */
+    public int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
+        int len = status.length;
+        // 记录盒子是否被拆开
+        boolean[] visited = new boolean[len];
+        // 记录我们拥有的所有盒子
+        Set<Integer> have_box = new HashSet<>();
+        // 记录我们拥有的所有钥匙
+        Set<Integer> have_key = new HashSet<>();
+        Queue<Integer> q = new LinkedList<>();
 
+        for (int i = 0; i < initialBoxes.length; i ++) {
+            int id = initialBoxes[i];
+            have_box.add(id);
 
+            if (status[id] == 1) {
+                q.offer(id);
+                visited[id] = true;
+            }
+        }
 
+        int total = 0;
+        while (!q.isEmpty()) {
+            int id = q.poll();
+            total += candies[id];
+
+            int[] curKeys = keys[id];
+            int[] curBoxes = containedBoxes[id];
+
+            for (int key: curKeys) {
+                have_key.add(key);
+                if (!visited[key] && have_box.contains(key)) {
+                    q.offer(key);
+                    visited[key] = true;
+                }
+            }
+
+            for (int boxId : curBoxes) {
+                have_box.add(boxId);
+                if (!visited[boxId] && (have_key.contains(boxId) || status[boxId] == 1)) {
+                    q.offer(boxId);
+                    visited[boxId] = true;
+                }
+            }
+        }
+
+        return total;
+    }
 
 
 
