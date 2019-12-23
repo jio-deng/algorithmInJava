99966070b33a77a275e9c5aa0b8b41fba57a5653
Johnny Deng <40154415+jio-deng@users.noreply.github.com>
2019-12-23 10:54:30 +0800

    [Add] LeetCode 017
    Signed-off-by: dengziming <501823206@qq.co>

---
+++ lib/src/main/java/com/dengzm/lib/leetcode/Leetcode017.java
@@ -0,0 +1,57 @@
+package com.dengzm.lib.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author Johnny Deng
+ * @version 1.0
+ * @description 电话号码的字母组合
+ * @date 2019/12/23 10:37
+ */
+public class Leetcode017 {
+    private HashMap<Integer, String> map = new HashMap<>();
+
+    {
+        map.put(2, "abc");
+        map.put(3, "def");
+        map.put(4, "ghi");
+        map.put(5, "jkl");
+        map.put(6, "mno");
+        map.put(7, "pqrs");
+        map.put(8, "tuv");
+        map.put(9, "wxyz");
+    }
+
+    public List<String> letterCombinations(String digits) {
+        ArrayList<String> result = new ArrayList<>();
+
+        if (digits == null || digits.length() == 0) {
+            return result;
+        }
+
+        letterCombCore(result, "", digits);
+
+        return result;
+    }
+
+    private void letterCombCore(ArrayList<String> result, String front, String digits) {
+        if (digits.equals("")) {
+            result.add(front);
+        } else {
+            int digit = digits.charAt(0) - '0';
+            String target = map.get(digit);
+            digits = digits.substring(1);
+
+            for (int i = 0; i < target.length(); i ++) {
+                letterCombCore(result, front + target.charAt(i), digits);
+            }
+        }
+    }
+
+    /*执行结果：通过
+      显示详情
+      执行用时 :1 ms, 在所有 java 提交中击败了95.05%的用户
+      内存消耗 :36.2 MB, 在所有 java 提交中击败了73.14%的用户*/
+}
