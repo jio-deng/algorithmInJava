43070ecb14a264212930e8bf0bec4ad59fb79c0c
Johnny Deng <40154415+jio-deng@users.noreply.github.com>
2020-01-22 12:59:21 +0800

    [Add] LeetCode 046 - 050
    Signed-off-by: dengziming <501823206@qq.co>

---
+++ lib/src/main/java/com/dengzm/lib/leetcode/lc/Leetcode049.java
@@ -0,0 +1,35 @@
+package com.dengzm.lib.leetcode.lc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author Johnny Deng
+ * @version 1.0
+ * @description 字母异位词分组
+ * @date 2020/1/22 12:27
+ */
+public class Leetcode049 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        if (strs == null || strs.length == 0) {
+            return result;
+        }
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = String.valueOf(chars);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+
+        result.addAll(map.values());
+        return result;
+    }
+}
