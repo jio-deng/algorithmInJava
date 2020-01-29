4c0097c8d967e9d1e4ccacd2d11d0971717e5d89
Johnny Deng <40154415+jio-deng@users.noreply.github.com>
2020-01-29 12:22:28 +0800

    [Add] LeetCode 73.74,77-80,82,83
    Signed-off-by: dengziming <501823206@qq.co>

---
+++ lib/src/main/java/com/dengzm/lib/leetcode/lc_51_100/Leetcode083.java
@@ -0,0 +1,25 @@
+package com.dengzm.lib.leetcode.lc_51_100;
+
+import com.dengzm.lib.leetcode.lc.ListNode;
+
+/**
+ * @author Johnny Deng
+ * @version 1.0
+ * @description 删除排序链表中的重复元素
+ * @date 2020/1/29 10:46
+ */
+public class Leetcode083 {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode first = head;
+
+        while (first != null && first.next != null) {
+            if (first.next.val == first.val) {
+                first.next = first.next.next;
+            } else {
+                first = first.next;
+            }
+        }
+
+        return head;
+    }
+}
