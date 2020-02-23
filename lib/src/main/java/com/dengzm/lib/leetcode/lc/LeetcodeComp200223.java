e47f47f07dca370319ad293df04e3fddf55eafb8
Johnny Deng <40154415+jio-deng@users.noreply.github.com>
2020-02-23 17:47:21 +0800

    [Add] LeetCode some code
    Signed-off-by: dengziming <501823206@qq.co>

--- lib/src/main/java/com/dengzm/lib/leetcode/lc/LeetcodeComp200223.java
+++ lib/src/main/java/com/dengzm/lib/leetcode/lc/LeetcodeComp200223.java
@@ -33,9 +33,25 @@ public class LeetcodeComp200223 {
      *
      * 给定的日期是 1971 年到 2100 年之间的有效日期。
      */
+    int[] x = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+
     public int daysBetweenDates(String date1, String date2) {
-        // TODO 这题有毒
-        return 0;
+        return Math.abs(get(date1)-get(date2));
+    }
+
+    int get(String date) {
+        int y=0,m=0,d=0,i,ans=0;
+
+        for(i=0;i<4;i++) y = y * 10 + date.charAt(i) - '0';
+        for(i=5;i<7;i++) m = m * 10 + date.charAt(i) - '0';
+        for(i=8;i<10;i++) d = d * 10 + date.charAt(i) - '0';
+        for(i=0;i<y;i++) if(i%400==0||i%4==0 && i%100!=0) ans+=366;
+        else ans+=365;
+        if(y%400==0 || y%4==0 && y%100!=0)x[2]=29;
+        else x[2]=28;
+        for(i=1;i<m;i++)ans+=x[i];
+        ans+=d;
+        return ans;
     }
 
     /**
